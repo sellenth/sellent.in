c256a2279b39ac0fa5a601d57942bf399b513e85
halston <sellenth@oregonstate.edu>
2022-10-27 17:08:16 +0700

    install problems on vercel

---
+++ etro/dist/layer/text.d.ts
@@ -0,0 +1,60 @@
+import { Dynamic, Color } from '../util';
+import { Visual, VisualOptions } from './visual';
+interface TextOptions extends VisualOptions {
+    text: Dynamic<string>;
+    font?: Dynamic<string>;
+    color?: Dynamic<Color>;
+    /** The text's horizontal offset from the layer */
+    textX?: Dynamic<number>;
+    /** The text's vertical offset from the layer */
+    textY?: Dynamic<number>;
+    maxWidth?: Dynamic<number>;
+    /**
+     * @desc The horizontal alignment
+     * @see [`CanvasRenderingContext2D#textAlign<`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textAlign)
+     */
+    textAlign?: Dynamic<string>;
+    /**
+     * @desc The vertical alignment
+     * @see [`CanvasRenderingContext2D#textBaseline`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textBaseline)
+     */
+    textBaseline?: Dynamic<string>;
+    /**
+     * @see [`CanvasRenderingContext2D#direction`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textBaseline)
+     */
+    textDirection?: Dynamic<string>;
+}
+declare class Text extends Visual {
+    text: Dynamic<string>;
+    font: Dynamic<string>;
+    color: Dynamic<Color>;
+    /** The text's horizontal offset from the layer */
+    textX: Dynamic<number>;
+    /** The text's vertical offset from the layer */
+    textY: Dynamic<number>;
+    maxWidth: Dynamic<number>;
+    /**
+     * @desc The horizontal alignment
+     * @see [`CanvasRenderingContext2D#textAlign<`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textAlign)
+     */
+    textAlign: Dynamic<string>;
+    /**
+     * @desc The vertical alignment
+     * @see [`CanvasRenderingContext2D#textBaseline`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textBaseline)
+     */
+    textBaseline: Dynamic<string>;
+    /**
+     * @see [`CanvasRenderingContext2D#direction`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textBaseline)
+     */
+    textDirection: Dynamic<string>;
+    private _prevText;
+    private _prevFont;
+    private _prevMaxWidth;
+    /**
+     * Creates a new text layer
+     */
+    constructor(options: TextOptions);
+    doRender(): void;
+    getDefaultOptions(): TextOptions;
+}
+export { Text, TextOptions };
---
+++ etro/dist/object.d.ts
@@ -0,0 +1,16 @@
+import { Movie } from './movie';
+/** A movie, layer or effect  */
+export default interface EtroObject {
+    /** Used in etro internals */
+    type: string;
+    /**
+     * Which properties to not watch for changes, for `Movie#autoRefresh`
+     *
+     * @deprecated `Movie#autoRefresh` is deprecated
+     */
+    publicExcludes: string[];
+    /** Map of property name to function to run on result of `val` */
+    propertyFilters: Record<string, <T>(value: T) => T>;
+    movie: Movie;
+    getDefaultOptions(): object;
+}
